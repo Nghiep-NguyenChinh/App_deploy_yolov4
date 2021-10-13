2ea8f0aeb523e87e42cd37351bc73cb95edb04df
Nguyễn Chính Nghiệp <72908486+Nghiep-NguyenChinh@users.noreply.github.com>
2021-10-14 01:27:03 +0700

    Update detection.py

--- detection.py
+++ detection.py
@@ -12,7 +12,7 @@ import streamlit.components.v1 as components
 ##### GIAO DIỆN
 #########################################################
 st.title("**OBJECT DETECTION**")
-st.markdown("<h1 style='text-align: center; color: red;'>OBJECT DETECTION</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>OBJECT DETECTION</h1>", unsafe_allow_html=True)
 
 st.header(" Menter: Huỳnh Trọng Nghĩa")
 st.subheader(" Mentees: Nguyễn Chính Nghiệp")
