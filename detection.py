52023f0e4e1acb28b0423c8c56c5bd214faac6e2
Nguyễn Chính Nghiệp <72908486+Nghiep-NguyenChinh@users.noreply.github.com>
2021-10-13 23:55:42 +0700

    Update detection.py

--- detection.py
+++ detection.py
@@ -1,9 +1,28 @@
 # Import libraries
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
-import glob
 import time
+import streamlit as st
+import requests, os
+from PIL import Image
+
+
+#Ham tải về
+##  Kiểm tra file 'name' có tồn tại chưa || không thì kéo nội dung từ link url tạo thành name 
+##
+def download(url, name):      
+    if (os.path.exists(name)==False):
+        st.write("Đang lấy file %s..." % name)
+        w = requests.get(url).content  # lấy nội dung url
+        with open(name,'wb') as f:
+            st.write(f.write(w))   # in ra màn hình
+        f.close()
+    else:
+        st.write("Đã tìm thấy file %s!" % name)
+
+download('https://archive.org/download/yolov4-custom_best_202110/yolov4-custom_best.weights', 'yolov4-custom_best.weights')
+download('https://archive.org/download/yolov4-custom_best_202110/yolov4-custom.cfg', 'yolov4-custom.cfg')
+download('https://archive.org/download/yolov4-custom_best_202110/yolo.names', 'yolo.names')
 
 def get_output_layers(net):
     layer_names = net.getLayerNames()
@@ -37,96 +56,96 @@ def savePredict(name, text):
 
 
 
+img_l = st.file_uploader("Upload Image",type=['jpg'])
+try:
+    img = Image.open(img_l)
+    image = np.array(img)
+    st.image(image, "Ảnh gốc")
+except: pass
 
-image = cv2.imread("car_test2.jpg")
-#image = cv2.imread("testing_images/vid_5_31700.jpg")
+btn = st.button("Băt đầu nhận diện")
+
+if btn:
 
 
 
-Width = image.shape[1]
-Height = image.shape[0]
-scale = 0.00392
+
+    Width = image.shape[1]
+    Height = image.shape[0]
+    scale = 0.00392
 
 
-classes = None
-with open("yolo.names", 'r') as f: # Edit CLASS file
-    classes = [line.strip() for line in f.readlines()]
+    classes = None
+    with open("yolo.names", 'r') as f: # Edit CLASS file
+        classes = [line.strip() for line in f.readlines()]
 
-COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
 
-net = cv2.dnn.readNet("yolov4-custom_best.weights", "yolov4-custom.cfg") # Edit WEIGHT and CONFIC file
-blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
+    net = cv2.dnn.readNet("yolov4-custom_best.weights", "yolov4-custom.cfg") # Edit WEIGHT and CONFIC file
+    blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
 
-net.setInput(blob)
-outs = net.forward(get_output_layers(net))
-#print(outs)
-class_ids = []
-confidences = []
-boxes = []
-conf_threshold = 0.2
-nms_threshold = 0.4
+    net.setInput(blob)
+    outs = net.forward(get_output_layers(net))
+    #print(outs)
+    class_ids = []
+    confidences = []
+    boxes = []
+    conf_threshold = 0.2
+    nms_threshold = 0.4
 
-start = time.time()
+    start = time.time()
 
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        #print(scores)
-        class_id = np.argmax(scores)
-        #print('b')
-        #print(class_id)
-        confidence = scores[class_id]
-        #print(confidence)
-        if confidence > 0.75:
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            #print(scores)
+            class_id = np.argmax(scores)
+            #print('b')
+            #print(class_id)
+            confidence = scores[class_id]
             #print(confidence)
-            center_x = int(detection[0] * Width)
-            center_y = int(detection[1] * Height)
-            w = int(detection[2] * Width)
-            h = int(detection[3] * Height)
-            x = center_x - w / 2
-            y = center_y - h / 2
-            #print(w,h,x,y)
-            class_ids.append(class_id)
-            """if confidence < 0.6:
-                class_ids.append(2)""" #change
-            confidences.append(float(confidence))
-            #print(confidence)
-            #print(class_ids)
-            boxes.append([x, y, w, h])
-            #print(boxes)
+            if confidence > 0.75:
+                #print(confidence)
+                center_x = int(detection[0] * Width)
+                center_y = int(detection[1] * Height)
+                w = int(detection[2] * Width)
+                h = int(detection[3] * Height)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                #print(w,h,x,y)
+                class_ids.append(class_id)
+#                 """if confidence < 0.6:
+#                     class_ids.append(2)""" #change
+                confidences.append(float(confidence))
+                #print(confidence)
+                #print(class_ids)
+                boxes.append([x, y, w, h])
+                #print(boxes)
 
-indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
 
-Result = ""
-for i in indices:
-    i = i[0]
-    box = boxes[i]
-    x = box[0]
-    y = box[1]
-    w = box[2]
-    h = box[3]
-    textpredict = "{} {} {}\n".format(str(class_ids[i]), x+ w/2, y+h/2)
-    #print(textpredict)
-    draw_prediction(image, class_ids[i],confidences[i], round(x), round(y), round(x + w), round(y + h))
-    Result += textpredict
-    #print(Result)
+    Result = ""
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        x = box[0]
+        y = box[1]
+        w = box[2]
+        h = box[3]
+        textpredict = "{} {} {}\n".format(str(class_ids[i]), x+ w/2, y+h/2)
+        #print(textpredict)
+        draw_prediction(image, class_ids[i],confidences[i], round(x), round(y), round(x + w), round(y + h))
+        Result += textpredict
+        #print(Result)
 
-file = open("testt.txt","w+")
-file.write(Result)
-file.close()
-#savePredict(pathSave, name, textPre) # Doi thanh con tro ve dia chi cua anh
     
-scale_percent = 50
-width = int(image.shape[1] * scale_percent / 100)
-height = int(image.shape[0] * scale_percent / 100)
-image = cv2.resize(src=image, dsize=(width,height))
-
-cv2.imshow("object detection", image)
+    scale_percent = 100
+    width = int(image.shape[1] * scale_percent / 100)
+    height = int(image.shape[0] * scale_percent / 100)
+    image = cv2.resize(src=image, dsize=(width,height))
 
 
-end = time.time()
-print("YOLO Execution time: " + str(end-start))
-cv2.waitKey()
+    end = time.time()
+    st.write("YOLO Execution time: " + str(end-start))
+    st.image(image, "Ảnh đã nhận diện")
 
-cv2.imwrite("object-detection.jpg", image)
-cv2.destroyAllWindows()
