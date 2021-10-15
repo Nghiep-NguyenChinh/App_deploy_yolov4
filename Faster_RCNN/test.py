197336306477ff5d831b74f4be198eeb05e68432
Nguyễn Chính Nghiệp <72908486+Nghiep-NguyenChinh@users.noreply.github.com>
2021-10-15 14:01:03 +0700

    Update test.py

--- Faster_RCNN/test.py
+++ Faster_RCNN/test.py
@@ -5,7 +5,7 @@ import numpy as np
 import sys
 import time
 from tensorflow.keras import backend as K
-from Faster_RCNN.keras_frcnn import roi_helpers
+from keras_frcnn import roi_helpers
 
 # Set learning phase to 0 for model.predict. Set to 1 for training
 K.set_learning_phase(0)
