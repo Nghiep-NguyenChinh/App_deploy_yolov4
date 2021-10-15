43b35974a5faeef749adb5f878c98e78f793bbda
Nguyễn Chính Nghiệp <72908486+Nghiep-NguyenChinh@users.noreply.github.com>
2021-10-15 12:39:52 +0700

    Update detection.py

--- detection.py
+++ detection.py
@@ -17,60 +17,36 @@ st.markdown("""
 
 |Menter   | Huỳnh Trọng Nghĩa  |
 |:-------:|:------------------:|
-| Mentees |Nghienp             |
-|         |Tùng                |
+| Mentees |Nguyễn Chính Nghiệp  |
+|         |Hà Sơn Tùng         |
 """
             
             , unsafe_allow_html=True)
 
-st.header(" Menter: Huỳnh Trọng Nghĩa")
-st.subheader(" Mentees: Nguyễn Chính Nghiệp")
-st.caption(" Mentees: Hà Sơn Tùng")
-components.html(
-    """
-<table style="border-collapse: collapse; width: 100%; height: 44px;" border="1">
-<tbody>
-<tr style="height: 17px;">
-<td style="width: 29.5407%; height: 17px;">Mentor</td>
-<td style="width: 70.4593%; height: 17px;"><span style="color: #ffffff;">Huỳnh Trung Nghĩa</span></td>
-</tr>
-<tr style="height: 17px;">
-<td style="width: 29.5407%; height: 17px;"><span style="color: #ffffff;">Mentees</span></td>
-<td style="width: 70.4593%; height: 17px;">Nguyễn Ch&iacute;nh Nghiệp</td>
-</tr>
-<tr style="height: 10px;">
-<td style="width: 29.5407%; height: 10px;">&nbsp;</td>
-<td style="width: 70.4593%; height: 10px;"><span style="color: #ffffff;">H&agrave; Sơn T&ugrave;ng</span></td>
-</tr>
-</tbody>
-</table>
-    """
-)
-
+st.write("")
 #Ham tải về
 ##  Kiểm tra file 'name' có tồn tại chưa || không thì kéo nội dung từ link url tạo thành name 
 ##
 def download(url, name):      
     if (os.path.exists(name)==False):
-        st.write("Đang lấy file %s..." % name)
+        #st.write("Đang lấy file %s..." % name)
         w = requests.get(url).content  # lấy nội dung url
         with open(name,'wb') as f:
             st.write(f.write(w))   # in ra màn hình
         f.close()
-    else:
-        st.write("Đã tìm thấy file %s!" % name)
+#     else:
+#         st.write("Đã tìm thấy file %s!" % name)
 
+#st.write("Đang lấy file weights...")
 download('https://archive.org/download/yolov4-custom_best_202110/yolov4-custom_best.weights', 'yolov4-custom_best.weights')
 download('https://archive.org/download/yolov4-custom_best_202110/yolov4-custom.cfg', 'yolov4-custom.cfg')
 download('https://archive.org/download/yolov4-custom_best_202110/yolo.names', 'yolo.names')
+st.write("Trạng thái: Sẵn sàng")
 
 
-######################################################
-###
-#######################################################
-option = st.selectbox('Chọn model',('Faster-RCNN', 'Yolov4', 'Mobile phone'))
-
+option = st.selectbox('Chọn model',('Faster-RCNN', 'Yolov4'))
 #st.write('You selected:', option)
+
 ##################################################################
 def get_output_layers(net):
     layer_names = net.getLayerNames()
@@ -102,86 +78,7 @@ def savePredict(name, text):
         groundTruth.close()
 
 
-def yolo(image):
-    Width = image.shape[1]
-    Height = image.shape[0]
-    scale = 0.00392
-
-
-    classes = None
-    with open("yolo.names", 'r') as f: # Edit CLASS file
-        classes = [line.strip() for line in f.readlines()]
-
-    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
-
-    net = cv2.dnn.readNet("yolov4-custom_best.weights", "yolov4-custom.cfg") # Edit WEIGHT and CONFIC file
-    blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
-
-    net.setInput(blob)
-    outs = net.forward(get_output_layers(net))
-    #print(outs)
-    class_ids = []
-    confidences = []
-    boxes = []
-    conf_threshold = 0.2
-    nms_threshold = 0.4
-
-    start = time.time()
-
-    for out in outs:
-        for detection in out:
-            scores = detection[5:]
-            #print(scores)
-            class_id = np.argmax(scores)
-            #print('b')
-            #print(class_id)
-            confidence = scores[class_id]
-            #print(confidence)
-            if confidence > 0.75:
-                #print(confidence)
-                center_x = int(detection[0] * Width)
-                center_y = int(detection[1] * Height)
-                w = int(detection[2] * Width)
-                h = int(detection[3] * Height)
-                x = center_x - w / 2
-                y = center_y - h / 2
-                #print(w,h,x,y)
-                class_ids.append(class_id)
-#                 """if confidence < 0.6:
-#                     class_ids.append(2)""" #change
-                confidences.append(float(confidence))
-                #print(confidence)
-                #print(class_ids)
-                boxes.append([x, y, w, h])
-                #print(boxes)
-
-    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
-
-    Result = ""
-    for i in indices:
-        i = i[0]
-        box = boxes[i]
-        x = box[0]
-        y = box[1]
-        w = box[2]
-        h = box[3]
-        textpredict = "{} {} {}\n".format(str(class_ids[i]), x+ w/2, y+h/2)
-        #print(textpredict)
-        draw_prediction(image, class_ids[i],confidences[i], round(x), round(y), round(x + w), round(y + h))
-        Result += textpredict
-        #print(Result)
-
-    
-    scale_percent = 100
-    width = int(image.shape[1] * scale_percent / 100)
-    height = int(image.shape[0] * scale_percent / 100)
-    image = cv2.resize(src=image, dsize=(width,height))
-
-
-    end = time.time()
-    st.write("YOLO Execution time: " + str(end-start))
-    st.image(image, "Ảnh đã nhận diện")
-
+  
 #################
 #### MAIN
 ################
@@ -192,12 +89,87 @@ try:
     st.image(image, "Ảnh gốc")
 except: pass
 
-btn = st.button("Băt đầu nhận diện")
+btn = st.button("Bắt đầu nhận diện")
 
 if btn:
-    if option=='Yolov4':
-        yolo(image)
+    if option=='Yolov4':        
+        Width = image.shape[1]
+        Height = image.shape[0]
+        scale = 0.00392
+        
+        classes = None
+        with open("yolo.names", 'r') as f: # Edit CLASS file
+            classes = [line.strip() for line in f.readlines()]
+
+        COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+
+        net = cv2.dnn.readNet("yolov4-custom_best.weights", "yolov4-custom.cfg") # Edit WEIGHT and CONFIC file
+        blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
+
+        net.setInput(blob)
+        outs = net.forward(get_output_layers(net))
+        #print(outs)
+        class_ids = []
+        confidences = []
+        boxes = []
+        conf_threshold = 0.2
+        nms_threshold = 0.4
+
+        start = time.time()
+
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                #print(scores)
+                class_id = np.argmax(scores)
+                #print('b')
+                #print(class_id)
+                confidence = scores[class_id]
+                #print(confidence)
+                if confidence > 0.75:
+                    #print(confidence)
+                    center_x = int(detection[0] * Width)
+                    center_y = int(detection[1] * Height)
+                    w = int(detection[2] * Width)
+                    h = int(detection[3] * Height)
+                    x = center_x - w / 2
+                    y = center_y - h / 2
+                    #print(w,h,x,y)
+                    class_ids.append(class_id)
+    #                 """if confidence < 0.6:
+    #                     class_ids.append(2)""" #change
+                    confidences.append(float(confidence))
+                    #print(confidence)
+                    #print(class_ids)
+                    boxes.append([x, y, w, h])
+                    #print(boxes)
+
+        indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+        Result = ""
+        for i in indices:
+            i = i[0]
+            box = boxes[i]
+            x = box[0]
+            y = box[1]
+            w = box[2]
+            h = box[3]
+            textpredict = "{} {} {}\n".format(str(class_ids[i]), x+ w/2, y+h/2)
+            #print(textpredict)
+            draw_prediction(image, class_ids[i],confidences[i], round(x), round(y), round(x + w), round(y + h))
+            Result += textpredict
+            #print(Result)
+
+
+        scale_percent = 100
+        width = int(image.shape[1] * scale_percent / 100)
+        height = int(image.shape[0] * scale_percent / 100)
+        image = cv2.resize(src=image, dsize=(width,height))
+
+
+        end = time.time()
+        st.write("YOLO Execution time: " + str(end-start))
+        st.image(image, "Ảnh đã nhận diện")
     elif option=='Faster-RCNN':
         st.write("Oke Rcnn")
     
-
